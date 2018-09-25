640c7c03001dbf3bb21d0bef5cfaaf0d66b644c3
PeterMc48 <42344164+PeterMc48@users.noreply.github.com>
2018-09-25 22:09:11 +0100

    book finished 25/9/18 22pm

--- src/TestBook.java
+++ src/TestBook.java
@@ -1,6 +1,8 @@
 public class TestBook {
     public static void main(String[] args) {
-        Book b1 = new Book();
+
+        Book b1 = new Book();//making an object from constructor
+
         System.out.println(b1);
 
     }
